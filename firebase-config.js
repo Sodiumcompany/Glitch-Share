29de3f2df6926eb2837c67313b0418272327ac38
Sodium Platform <sajidulhasan2025+@gmail.com>
2026-08-13 16:57:45 +0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,13 @@
 // firebase-config.js
 const firebaseConfig = {
-  apiKey: "AIzaSyAV_A3YOv6DAVSF02hmdJnjS9ieckYbdKI",
-  authDomain: "glitchi-share.firebaseapp.com",
-  projectId: "glitchi-share",
-  storageBucket: "glitchi-share.firebasestorage.app",
-  messagingSenderId: "252725027131",
-  appId: "1:252725027131:web:54c3afa17fa19fdf13971e",
-  measurementId: "G-EZ4W14R9K4"
+  apiKey: "AIzaSyCVzABKzgST2b-0D53m7hFmk-yv_BFLadg",
+  authDomain: "glitchi-share-b1a7d.firebaseapp.com",
+  databaseURL: "https://glitchi-share-b1a7d-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "glitchi-share-b1a7d",
+  storageBucket: "glitchi-share-b1a7d.firebasestorage.app",
+  messagingSenderId: "848667519666",
+  appId: "1:848667519666:web:19d70451c2ba44f4a977ab",
+  measurementId: "G-T84Z05VXZZ"
 };
 
 // Initialize Firebase
